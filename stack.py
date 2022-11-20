1205a7748baacda97616777e463ff5b9c5bbd7fa
Amir Faysal <amirfaysal0471@gmail.com>
2022-11-20 12:39:20 +0600

    it is a stack file

---
+++ stack.py
@@ -0,0 +1,20 @@
+stack =[]
+stack.append("Physics")
+stack.append("Math")
+stack.append("CSE")
+
+
+
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
+
+stack.pop()
+print(stack)
+
+if not stack:
+    print("Empty Element")
